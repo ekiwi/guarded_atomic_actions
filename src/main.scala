a9b6fd19de1d788c0f5e7196afe7aea100b673fc
Kevin Läufer <laeufer@cs.berkeley.edu>
2018-11-11 15:46:51 -0800

    main: generate verilog

--- src/main.scala
+++ src/main.scala
@@ -4,9 +4,12 @@ import chisel3.util._
 object Main {
     def main(args: Array[String]): Unit = {
 
-
+        // print firrtl to command line
         val ir = chisel3.Driver.elaborate(() => new ReferenceGcd(4))
         val firrtl = chisel3.Driver.emit(ir)
         println(firrtl)
+
+        // generate verilog and save to file
+        chisel3.Driver.execute(args, () => new ReferenceGcd(4))
     }
 }
