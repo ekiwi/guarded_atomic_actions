2e10aed9a276e2d6818fc27e695826ed075fcece
Kevin Läufer <laeufer@cs.berkeley.edu>
2018-11-11 16:56:26 -0800

    split into gaa suport library and gcd implementation

---
+++ src/gcd.scala
@@ -0,0 +1,29 @@
+import chisel3._
+import chisel3.util._
+
+class GuardedAtomicActionGcd(width: Int) extends GaaModule {
+    // state elements
+    val x = Reg(UInt(width.W))
+    val y = RegInit(0.U(width.W))
+
+    // helper expression
+    val is_active : Bool = y =/= 0.U
+
+    rule ("swap") .when(x > y && is_active) {
+        x := y
+        y := x
+    }
+
+    rule ("subtract") .when(x <= y && is_active) {
+        y := y - x
+    }
+
+    action ("start") .arg("a", UInt(width.W)) .arg("b", UInt(width.W)) .when(!is_active) {
+        x := arg("a")
+        y := arg("b")
+    }
+
+    value ("result") .when(!is_active) {
+        x
+    }
+}
